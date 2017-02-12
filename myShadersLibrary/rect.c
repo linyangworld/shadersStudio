fd4913d93ac52395c46c262225fc9ffc062f44af
linyang <dorisyang.sysu@gmail.com>
2017-02-12 17:14:15 -0500

    Update rect.c

--- myShadersLibrary/rect.c
+++ myShadersLibrary/rect.c
@@ -3,10 +3,9 @@ precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform float u_time;
 
-float rectSDF(vec2 st, vec2 s) {
+float rect(vec2 st, vec2 s) {
     st = st*2.-1.;
     return max( abs(st.x/s.x),
                 abs(st.y/s.y));
@@ -22,7 +21,7 @@ void main() {
     st.x *= u_resolution.x/u_resolution.y;
     vec3 color = vec3(.0); 
     
-	color += fill(rectSDF(st,vec2 (0.8)), .5);
+	color += fill(rect(st,vec2 (0.8)), .5);
     
 
     gl_FragColor = vec4(color,1.0);
